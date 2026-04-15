70216cc5327059c461cbf592ab297f2d1e7983a8
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-15 12:43:25 +0530

    add navbar ,

---
+++ Contrast-Academy-fe/src/app/features/dashboard/components/course-cards/course-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-cards.component.html',
+  styleUrl: './course-cards.component.css'
+})
+export class CourseCardsComponent {
+
+}
--- Contrast-Academy-fe/src/app/features/dashboard/pages/dashboard.page/dashboard.page.component.ts
+++ Contrast-Academy-fe/src/app/features/dashboard/pages/dashboard.page/dashboard.page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { StatsCardComponent } from "../../components/stats-card/stats-card.component";
+import { NavbarComponent } from "../../../../layout/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard.page',
   standalone: true,
-  imports: [StatsCardComponent],
+  imports: [StatsCardComponent, NavbarComponent],
   templateUrl: './dashboard.page.component.html',
   styleUrl: './dashboard.page.component.css'
 })
