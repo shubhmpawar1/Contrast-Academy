b0245478933c669a1794d64a87ad2a25128cba03
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-15 11:16:09 +0530

    add stata card

--- Contrast-Academy-fe/src/app/app.routes.ts
+++ Contrast-Academy-fe/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardPageComponent } from './features/dashboard/pages/dashboard.page/dashboard.page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: DashboardPageComponent
+    }
+];
--- Contrast-Academy-fe/src/app/features/dashboard/pages/dashboard.page/dashboard.page.component.ts
+++ Contrast-Academy-fe/src/app/features/dashboard/pages/dashboard.page/dashboard.page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StatsCardComponent } from "../../components/stats-card/stats-card.component";
 
 @Component({
   selector: 'app-dashboard.page',
   standalone: true,
-  imports: [],
+  imports: [StatsCardComponent],
   templateUrl: './dashboard.page.component.html',
   styleUrl: './dashboard.page.component.css'
 })
