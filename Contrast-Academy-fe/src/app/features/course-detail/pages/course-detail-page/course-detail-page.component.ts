a98cdbcb16cac87bb9c22b7d939abbd82a726f99
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-16 18:13:47 +0530

    add new page course-detail

---
+++ Contrast-Academy-fe/src/app/features/course-detail/pages/course-detail-page/course-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-detail-page.component.html',
+  styleUrl: './course-detail-page.component.css'
+})
+export class CourseDetailPageComponent {
+
+}
