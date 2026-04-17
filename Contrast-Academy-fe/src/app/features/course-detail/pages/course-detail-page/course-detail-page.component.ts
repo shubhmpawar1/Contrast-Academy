be208988db656a3ee88e0f1d332ed4d827fb276c
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-17 10:37:21 +0530

    add nav bar in ciurse-detail

--- Contrast-Academy-fe/src/app/features/course-detail/pages/course-detail-page/course-detail-page.component.ts
+++ Contrast-Academy-fe/src/app/features/course-detail/pages/course-detail-page/course-detail-page.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseDetailService, CourseDetail } from '../../services/course-detail.service';
 import { CourseDetailHeroSectionComponent } from '../../componet/course-detail-hero-section/course-detail-hero-section.component';
+import { NavbarComponent } from "../../../../layout/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-course-detail-page',
   standalone: true,
-  imports: [CommonModule, CourseDetailHeroSectionComponent],
+  imports: [CommonModule, CourseDetailHeroSectionComponent, NavbarComponent],
   templateUrl: './course-detail-page.component.html',
   styleUrl: './course-detail-page.component.css'
 })
