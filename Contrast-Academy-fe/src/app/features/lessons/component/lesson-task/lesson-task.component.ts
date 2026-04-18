6af8765a6f96479d381f6727afcb0cd4dfd24003
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-18 14:47:47 +0530

    add task timer

--- Contrast-Academy-fe/src/app/features/lessons/component/lesson-task/lesson-task.component.ts
+++ Contrast-Academy-fe/src/app/features/lessons/component/lesson-task/lesson-task.component.ts
@@ -1,13 +1,55 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { LessonContent } from '../../services/lessons.service';
 
 @Component({
   selector: 'app-lesson-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lesson-task.component.html',
   styleUrl: './lesson-task.component.css'
 })
-export class LessonTaskComponent {
+export class LessonTaskComponent implements OnDestroy {
   @Input() lesson!: LessonContent;
+
+  isTimerRunning: boolean = false;
+  timeElapsed: number = 0; // In seconds
+  timerInterval: any;
+
+  ngOnDestroy(): void {
+    this.pauseTimer();
+  }
+
+  toggleTimer(): void {
+    if (this.isTimerRunning) {
+      this.pauseTimer();
+    } else {
+      this.startTimer();
+    }
+  }
+
+  startTimer(): void {
+    this.isTimerRunning = true;
+    this.timerInterval = setInterval(() => {
+      this.timeElapsed++;
+    }, 1000);
+  }
+
+  pauseTimer(): void {
+    this.isTimerRunning = false;
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval);
+    }
+  }
+
+  resetTimer(): void {
+    this.pauseTimer();
+    this.timeElapsed = 0;
+  }
+
+  get formattedTime(): string {
+    const minutes = Math.floor(this.timeElapsed / 60);
+    const seconds = this.timeElapsed % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
 }
