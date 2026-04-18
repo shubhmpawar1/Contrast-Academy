d2ffac1c807f58fa4ed0d05aec9d1154d4e36944
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-18 15:26:26 +0530

    add login and register page

--- Contrast-Academy-fe/src/app/app.routes.ts
+++ Contrast-Academy-fe/src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { DashboardPageComponent } from './features/dashboard/pages/dashboard.pag
 import { CourseCatalogPageComponent } from './features/course-catalog/pages/course-catalog/course-catalog.page';
 import { CourseDetailPageComponent } from './features/course-detail/pages/course-detail-page/course-detail-page.component';
 import { LessonsPageComponent } from './features/lessons/pages/lessons-page/lessons-page.component';
+import { RegisterPageComponent } from './features/auth/pages/register-page/register-page/register-page.component';
+import { LoginPageComponent } from './features/auth/pages/login-page/login-page/login-page.component';
 
 export const routes: Routes = [
     {
-        path: '', component: DashboardPageComponent
+        path: '', component: RegisterPageComponent
+    },
+    {
+        path: 'register', component: RegisterPageComponent
+    },
+    {
+        path: 'login', component: LoginPageComponent
     },
     {
         path: 'courses', component: CourseCatalogPageComponent
