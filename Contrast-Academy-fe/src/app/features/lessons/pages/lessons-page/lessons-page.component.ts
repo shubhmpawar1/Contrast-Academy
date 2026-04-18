59934a179f6d43eda69ef6ad635ef4fcea6c1bd4
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-18 11:16:50 +0530

    create page and services

---
+++ Contrast-Academy-fe/src/app/features/lessons/pages/lessons-page/lessons-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lessons-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './lessons-page.component.html',
+  styleUrl: './lessons-page.component.css'
+})
+export class LessonsPageComponent {
+
+}
