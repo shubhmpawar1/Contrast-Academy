d1b0475f4d874a4b1e318c4c024078f81b01df48
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-18 14:21:20 +0530

    add correct or incorrect in quiz

--- Contrast-Academy-fe/src/app/features/lessons/component/lessons-quick-quiz/lessons-quick-quiz.component.ts
+++ Contrast-Academy-fe/src/app/features/lessons/component/lessons-quick-quiz/lessons-quick-quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LessonContent } from '../../services/lessons.service';
 
@@ -9,6 +9,33 @@ import { LessonContent } from '../../services/lessons.service';
   templateUrl: './lessons-quick-quiz.component.html',
   styleUrl: './lessons-quick-quiz.component.css'
 })
-export class LessonsQuickQuizComponent {
+export class LessonsQuickQuizComponent implements OnChanges {
   @Input() lesson!: LessonContent;
+  
+  selectedOption: string | null = null;
+  isSubmitted: boolean = false;
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['lesson']) {
+      // Reset quiz state when we navigate to a new lesson
+      this.selectedOption = null;
+      this.isSubmitted = false;
+    }
+  }
+
+  selectOption(option: string): void {
+    if (!this.isSubmitted) {
+      this.selectedOption = option;
+    }
+  }
+
+  submitAnswer(): void {
+    if (this.selectedOption) {
+      this.isSubmitted = true;
+    }
+  }
+
+  get isCorrect(): boolean {
+    return this.selectedOption === this.lesson.quiz.correctAnswer;
+  }
 }
