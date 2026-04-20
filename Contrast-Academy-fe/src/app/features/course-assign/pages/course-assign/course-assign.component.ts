c84f2521e2a00b8fbfb4463949011b45489f4688
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-20 11:06:24 +0530

    add course-assign page

---
+++ Contrast-Academy-fe/src/app/features/course-assign/pages/course-assign/course-assign.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-assign',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-assign.component.html',
+  styleUrl: './course-assign.component.css'
+})
+export class CourseAssignComponent {
+
+}
